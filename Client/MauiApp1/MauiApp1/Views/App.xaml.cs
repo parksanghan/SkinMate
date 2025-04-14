bb430c9d0e7d8970e6662458420936f4b6cacb4b
a_crszkvc30Last_NameCol <68534093+parksanghan@users.noreply.github.com>
2025-04-14 23:12:40 +0900

    app init
    app init

--- Client/MauiApp1/MauiApp1/Views/App.xaml.cs
+++ Client/MauiApp1/MauiApp1/Views/App.xaml.cs
@@ -1,10 +1,14 @@
-﻿namespace MauiApp1.Views;
+﻿using MauiApp1.Services;
+
+namespace MauiApp1.Views;
 
 public partial class App : Application
 {
     public App()
     {
         InitializeComponent();
+        //  앱 시작시 context 초기화
+        HttpService.Instance.ContextInit();
     }
 
     protected override Window CreateWindow(IActivationState? activationState)
