8407392f8fbde2861bddbcdbc2fe9e4699990fa8
a_crszkvc30Last_NameCol <68534093+parksanghan@users.noreply.github.com>
2025-04-16 22:57:34 +0900

    shell init
    shell init

--- Client/MauiApp1/MauiApp1/Views/App.xaml.cs
+++ Client/MauiApp1/MauiApp1/Views/App.xaml.cs
@@ -14,5 +14,7 @@ public partial class App : Application
     protected override Window CreateWindow(IActivationState? activationState)
     {
         return new Window(new Login_RegisterPage());    
+        // 처음시작시에는 로그인 페이지만 진입
+        // 이후 로그인 성공 시 contextinit과 appshell 진입
     }
 }
--- Client/MauiApp1/MauiApp1/Views/AppShell.xaml.cs
+++ Client/MauiApp1/MauiApp1/Views/AppShell.xaml.cs
@@ -5,5 +5,9 @@ public partial class AppShell : Shell
     public AppShell()
     {
         InitializeComponent();
+        Routing.RegisterRoute(nameof(MainPage), typeof(MainPage));
+        Routing.RegisterRoute(nameof(ChatViewPage), typeof(ChatViewPage));
+        Routing.RegisterRoute(nameof(CameraViewPage), typeof(CameraViewPage));
+        Routing.RegisterRoute(nameof(HistoryViewPage), typeof(HistoryViewPage));
     }
 }
