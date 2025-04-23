ef0cc068a2f5132b492c959d810d6ca408022573
a_crszkvc30Last_NameCol <68534093+parksanghan@users.noreply.github.com>
2025-04-23 17:26:50 +0900

    Diagnosis Result
    Diagnosis Result

---
+++ Client/MauiApp1/MauiApp1/Data/DiagnosisResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Data
+{
+    public class DiagnosisResult
+    {
+        public DiagnosisClassification? Classification {  get; set; }  
+        public DiagnosisRegression? Regression { get; set; }    
+
+        public DiagnosisResult(DiagnosisClassification? classification, DiagnosisRegression? regression )
+        {
+            Classification = classification;
+            Regression= regression;
+        }
+    }
+}
--- Client/MauiApp1/MauiApp1/obj/Debug/net9.0-maccatalyst/maccatalyst-x64/MauiApp1.AssemblyInfo.cs
+++ Client/MauiApp1/MauiApp1/obj/Debug/net9.0-maccatalyst/maccatalyst-x64/MauiApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e917c55e0ed9cd9bdf5f58c046ca25715412d8c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e9fbb157acc4e1d4a8af1aad7c6ca60f824f7a66")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
