f1f4c2014fcb6ac4d9d3261b1a98f8fca3ee1a22
a_crszkvc30Last_NameCol <68534093+parksanghan@users.noreply.github.com>
2025-05-11 20:06:19 +0900

    winversion

---
+++ Client/MauiApp1/MauiApp1/Views/ChatViewPageWin.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp1.Views;
+
+public partial class ChatViewPageWin : ContentPage
+{
+	public ChatViewPageWin()
+	{
+		InitializeComponent();
+	}
+}
--- Client/MauiApp1/MauiApp1/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MauiApp1.AssemblyInfo.cs
+++ Client/MauiApp1/MauiApp1/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MauiApp1.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+cea235c2bb14334725ce39590d2263535ccaf626")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+52cc2471f1dedf5c1634243d233c9e776fc635de")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
