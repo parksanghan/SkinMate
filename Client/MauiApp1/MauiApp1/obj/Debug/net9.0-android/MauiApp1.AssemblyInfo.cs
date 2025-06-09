b070ea21024278dc5502fbf57f4a3e833d61d03e
a_crszkvc30Last_NameCol <68534093+parksanghan@users.noreply.github.com>
2025-06-10 03:03:55 +0900

    dll

--- Client/MauiApp1/MauiApp1/obj/Debug/net9.0-android/MauiApp1.AssemblyInfo.cs
+++ Client/MauiApp1/MauiApp1/obj/Debug/net9.0-android/MauiApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0f9224d90c732a74c3ba803b491a27b5e63cb688")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+5ca7fdbeacf6a73496d4ef2b86ab1d01e8ecb0bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
